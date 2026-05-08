fef069e34a9a1aac4df85064a48a5b0e2081f320
Sirus <5435333+Sirush@users.noreply.github.com>
2026-05-08 21:54:56 +0200

    Add anime source API interfaces and models

---
+++ source-api/src/commonMain/kotlin/eu/kanade/tachiyomi/animesource/model/AnimeUpdateStrategy.kt
@@ -0,0 +1,7 @@
+package eu.kanade.tachiyomi.animesource.model
+
+@Suppress("UNUSED")
+enum class AnimeUpdateStrategy {
+    ALWAYS_UPDATE,
+    ONLY_FETCH_ONCE,
+}
---
+++ source-api/src/commonMain/kotlin/eu/kanade/tachiyomi/animesource/model/SEpisodeImpl.kt
@@ -0,0 +1,16 @@
+@file:Suppress("PropertyName")
+
+package eu.kanade.tachiyomi.animesource.model
+
+class SEpisodeImpl : SEpisode {
+
+    override lateinit var url: String
+
+    override lateinit var name: String
+
+    override var date_upload: Long = 0
+
+    override var episode_number: Float = -1f
+
+    override var scanlator: String? = null
+}
